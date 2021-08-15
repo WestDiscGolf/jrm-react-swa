ecca386cd74404a8268d3bb37b35a00b82f587cc
Tim Wong <k1ck3rtw@gmail.com>
2021-08-14 20:35:38 -0400

    test change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}. We're running react!</div>;
 }
 
 export default App;
