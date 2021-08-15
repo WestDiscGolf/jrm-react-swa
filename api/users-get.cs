fff220dd60ba0d78a0127a0b26f589969ee9c209
Tim Wong <k1ck3rtw@gmail.com>
2021-08-15 11:17:37 -0400

    Getting Environment Variables

--- api/users-get.cs
+++ api/users-get.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace JRM.Functions
 {
@@ -17,8 +14,10 @@ namespace JRM.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "users")] HttpRequest req,
             ILogger log)
         {
+            var testEnvValue = Utils.Utils.GetEnvironmentVariable("Test_Env_Variable");
+
             log.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("This endpoint should return all users");
+            return new OkObjectResult($"This endpoint should return all users - {testEnvValue}");
         }
     }
 }
---
+++ api/utils/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JRM.Functions.Utils
+{
+    public static class Utils
+    {
+        public static string GetEnvironmentVariable(string name)
+        {
+            return name + ": " +
+                Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
