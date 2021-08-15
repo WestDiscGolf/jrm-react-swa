2b6277883114c6f7a28526a2fb90b945d6d997ed
Tim Wong <k1ck3rtw@gmail.com>
2021-08-15 19:51:56 -0400

    protecting routes + google login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import React from 'react';
 
 function App() {
+  const redirect = window.location.pathname;
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      //console.error('No profile could be found');
+      return undefined;
+    }
+  }
+
   const value = 'World';
-  return <div>Hello {value}. We're running react!</div>;
+  return (
+    <div>
+      Hello {value}. We're running react! <br/><br/>
+
+      {!userInfo && (
+        <>
+        <a href={`/.auth/login/google?post_login_redirect_uri=${redirect}`}>Google Login</a><br/>
+        </>
+      )}      
+      
+      {userInfo && (
+        <div>
+          <div className="user">
+            <p>Welcome</p>
+            <p>{userInfo && userInfo.userDetails}</p>
+            <p>{userInfo && userInfo.identityProvider}</p>
+          </div>
+          <a href="/.auth/logout">Log out</a>
+        </div>
+      )}      
+    </div>
+  );
 }
 
 export default App;
