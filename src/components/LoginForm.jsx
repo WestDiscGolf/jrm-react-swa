25a0de048c84ecc1cef0cd5be01b6f4ca1288486
Tim Wong <k1ck3rtw@gmail.com>
2021-08-29 22:21:26 -0400

    remove unneeded code

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './LoginForm.css';
@@ -17,29 +17,6 @@ function LoginForm() {
         </Card.ImgOverlay>
       </Card>
     </div>
-    // <main class="form-signin">
-    //   <form>
-    //     <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-    //     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-    
-    //     <div class="form-floating">
-    //       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-    //       <label for="floatingInput">Email address</label>
-    //     </div>
-    //     <div class="form-floating">
-    //       <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-    //       <label for="floatingPassword">Password</label>
-    //     </div>
-    
-    //     <div class="checkbox mb-3">
-    //       <label>
-    //         <input type="checkbox" value="remember-me" /> Remember me
-    //       </label>
-    //     </div>
-    //     <Button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</Button>
-    //     <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-    //   </form>
-    // </main>
   );
 }
 
